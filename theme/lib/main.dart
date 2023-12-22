487418ea2fd4ab2bf5a1cae8df801322a65ad3c5
kelvinleonardos <kelvinlsianipar@gmail.com>
2023-12-22 22:50:11 +0800

    theme

---
+++ theme/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Theme"),
+      ),
+      body: const Center(
+        child: Text("This is a text"),
+      ),
+    );
+  }
+}
